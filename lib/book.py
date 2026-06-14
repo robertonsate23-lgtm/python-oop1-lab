c584cb388552378f61f99934c858286a86f4ceaf
roberte <robertonsate23@gmail.com>
2026-06-14 14:06:06 +0300

    feat: complete book and coffee classes with passing tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        # Call the setter to enforce type validation during initialization
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+            # Setting it to None or keeping it unassigned prevents invalid types
+            self._page_count = None 
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
